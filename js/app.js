ff04ebd835ea0e07fd9f6ab8792b9a9ab9c63520
Oleksii <oleksiibuntikov5@gmail.com>
2023-02-23 03:40:10 +0200

    start js

---
+++ js/app.js
@@ -0,0 +1,3 @@
+let firstNumber = +prompt('Введите первое число');
+let secondNumber = +prompt('Введите второе число');
+document.write(`Сумма ${firstNumber + secondNumber}, разница ${firstNumber - secondNumber}, умножение ${firstNumber * secondNumber}, деление ${firstNumber / secondNumber}`);
